d97c67796dd7f0306a61d161d7b41f59e7c5870d
kaliyaperumal <rkaliss@gmail.com>
2016-12-11 17:16:17 +0530

    Update onos_setup.sh
    moved notes on creating application to setup.txt

--- onos/setup/onos_setup.sh
+++ onos/setup/onos_setup.sh
@@ -10,29 +10,3 @@ export ONOS_IP=10.76.190.84
 #mvn clean install -DskipTests -Dcheckstyle.skip
 #cat out.txt  | python -m json.tool > out.pretty
 
-
-
-[INFO] Archetype repository not defined. Using the one from [org.onosproject:onos-bundle-archetype:1.7.1] found in catalog remote
-Define value for property 'groupId': : com.verizon.onos.app
-Define value for property 'artifactId': : verizon-app
-Define value for property 'version':  1.0-SNAPSHOT: : 1.5.0
-Define value for property 'package':  com.verizon.onos.app: : 
-Confirm properties configuration:
-groupId: com.verizon.onos.app
-artifactId: verizon-app
-version: 1.5.0
-package: com.verizon.onos.app
- Y: : Y
-[INFO] ----------------------------------------------------------------------------
-[INFO] Using following parameters for creating project from Archetype: onos-bundle-archetype:1.5.0
-[INFO] ----------------------------------------------------------------------------
-[INFO] Parameter: groupId, Value: com.verizon.onos.app
-[INFO] Parameter: artifactId, Value: verizon-app
-[INFO] Parameter: version, Value: 1.5.0
-[INFO] Parameter: package, Value: com.verizon.onos.app
-[INFO] Parameter: packageInPathFormat, Value: com/verizon/onos/app
-[INFO] Parameter: package, Value: com.verizon.onos.app
-[INFO] Parameter: version, Value: 1.5.0
-[INFO] Parameter: groupId, Value: com.verizon.onos.app
-[INFO] Parameter: artifactId, Value: verizon-app
-[INFO] project created from Archetype in dir: /home/verizon/onos/ONOS/onos/verizon-app
